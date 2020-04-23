96ce0918ce9aaaed72eb233d27b0020dff5c0490
huoyingjie <1210539847@qq.com>
2020-04-23 15:19:51 +0800

    2020/4/23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MENUS, PRODUCTS } from './data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'try';
+   menus = MENUS;
+   products = PRODUCTS;
 }
